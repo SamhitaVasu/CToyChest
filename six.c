b84719e4363ba5a6e94b074f1e16e168034f3710
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-17 18:47:34 -0700

    First version

---
+++ six.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+   int i = 0;
+   printf("PLease enter a number: ");
+   scanf("%d",&i);
+   printf("%d\n", i);
+   i++;
+   printf("%d\n", i);
+   i--;
+   printf("%d\n", i);
+}
+
