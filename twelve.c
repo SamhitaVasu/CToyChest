75cc9797a78dc9efb4ea4a9e8370936764fed22e
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-17 18:48:14 -0700

    First version

---
+++ twelve.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int n = 0;
+   printf("Please enter a number: ");
+   scanf("%d", &n);
+   int number = 1;
+   int j = 1;
+   do {
+      number = number*j;
+      j++;
+   }while(j <= n);
+   printf("%d\n", number);
+}
