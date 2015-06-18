6d6a7daf7b642cda4a5ad9ae28c78149507e045d
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-17 18:51:01 -0700

    first version

---
+++ toyinprogress.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+   int i = 0;
+   printf("Please enter your age");
+   scanf("%d", &i);
+   int age = i;
+   printf("age is %d", age);
+   return 0;
+}
+   void hello()
+   {
+      static int j = 0;
+      if (age > 12) 
+      {
+         age--;
+         j--;
+         hello();
+      }
+      else if (age < 12)
+      {
+         age++;
+         j++;
+         hello();
+      }
+      else
+      {
+         printf("Our age difference is\n");
+         printf(j);
+         printf("years");
+      }
+   }
