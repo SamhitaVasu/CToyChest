ff06e623dab0fbf9c8e7857dfb6086026308e42a
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-17 18:48:50 -0700

    First version

---
+++ seventeen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{   
+   char month[10];
+   month[0] = 'J';
+   month[1] = 'u';
+   month[2] = 'n';
+   month[3] = 'e';
+   month[4] = '\0';
+   printf("%c %c %c %c \n", month[0], month[1], month[2], month[3]);
+   printf("%s\n", month);
+}
