53bf018da22aa57d126a97b04c6d14ce59428231
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-17 18:47:40 -0700

    First version

---
+++ seven.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+   int i = 0;
+   int cnt = 0;
+   printf("PLease enter a number");
+   scanf("%d",&i);
+   printf("PLease enter a loop count");
+   scanf("%d",&cnt);
+   printf("%d\n", i);
+   for(int j = 0; j < cnt; j++)
+   {
+      i++;
+   }
+   printf("After the ++, i is %d\n", i);
+}
+
