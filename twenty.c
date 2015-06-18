7401e96158d7ab8e1e486a1409101228330b3579
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-17 18:49:21 -0700

    First version

---
+++ twenty.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int findingi()
+{
+   int i;
+   printf("Choose a number between 1 and 20: ");
+   scanf("%d", &i);
+   return i;
+}
+
+int main()
+{
+   switch(findingi())
+   {
+      case 1 ... 9:
+         printf("This is a small number\n");
+         break;
+      case 10 ... 15:
+         printf("This is a medium number\n");
+         break;
+      case 16 ... 20:
+         printf("This is a large number\n");
+         break;
+      default:
+         printf("Try again\n");
+         break;
+   }
+} 
