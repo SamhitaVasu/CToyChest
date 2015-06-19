c622d24b697d18500856af94a5eca732f9673b32
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-18 18:29:37 -0700

    first version

--- ten.c
+++ ten.c
@@ -6,9 +6,11 @@ int main()
    printf("Please enter a number: ");
    scanf("%d", &n);
    int number = 1;
-   for(int j = 1; j<=n; j++)
+   for(int j = 1; j<=n; j++) //This is a for loop
    {
       number = number*j;
    }
    printf("%d\n", number);
 }
+
+// This program uses a for loop to find the factorial of a number of your choice
