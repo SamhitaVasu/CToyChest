b8f5f52102bf7c79fc9d72f1b1d5780d1639c310
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-18 18:29:21 -0700

    first version

--- seven.c
+++ seven.c
@@ -4,9 +4,9 @@ int main()
 {
    int i = 0;
    int cnt = 0;
-   printf("PLease enter a number");
+   printf("PLease enter a number: ");
    scanf("%d",&i);
-   printf("PLease enter a loop count");
+   printf("PLease enter a loop count: ");
    scanf("%d",&cnt);
    printf("%d\n", i);
    for(int j = 0; j < cnt; j++)
@@ -16,3 +16,4 @@ int main()
    printf("After the ++, i is %d\n", i);
 }
 
+// This program takes two integers of your choice and adds them together
