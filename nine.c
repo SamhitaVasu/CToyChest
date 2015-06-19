8a8c1c83ba512746e5a42495541e682420d29cd7
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-18 18:29:32 -0700

    first version

--- nine.c
+++ nine.c
@@ -7,13 +7,15 @@ int main()
    scanf("%d", &i);
    if (i < 100)
    {
-      printf("Your number is too small\n");
+      printf("Your number is too small\n"); //if i is less than 100, it is too small
    }
-   else if (i > 1000)
+   else if (i > 1000)   
    {
-      printf("Your number is too large\n");
+      printf("Your number is too large\n"); //if i is greater than 1000, it is too large
    }
    else {
-      printf("Your number is just right\n");
+      printf("Your number is just right\n"); //if i is in between 100 and 1000, it is just right
    }
 }
+
+//This program tells whether the number you chose is too small, too large, or just right
