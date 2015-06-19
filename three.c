6d89900770c568d3c3f13e75d1a9cc9f70d51504
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-18 18:29:03 -0700

    first version

--- three.c
+++ three.c
@@ -5,7 +5,9 @@ int main()
    float f = 0.0;
    printf("PLease enter a float: ");
    scanf("%f",&f);
-   printf("%f\n", f);
+   printf("%f\n", f); // "\n" means new line
    printf("Hello World!\n");
-}
+} 
+
+//This program prints the float you types along with "Hello World"
 
