a22077752758d03514f83500d65efd95ff2ce519
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-24 17:49:56 -0700

    first version

---
+++ thirtyfour.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+   
+   printf("%s is %s\n", argv[1], argv[2]);
+   return 0;
+}
+
+//This program uses command line arguments to make a sentence
