b155711873033574338ee6443646a215146042c0
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-24 17:49:59 -0700

    first version

---
+++ thirtyfive.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int i = 12, j = 10;
+   double n;
+   n = (double) i/j;
+   printf("%f\n", n);
+}
+
+//This program uses typecast to find the exact quotient of two nondivisible numbers
