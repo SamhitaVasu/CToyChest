b1bae1cf8a9e6cf92b7e0697fb8f29a326306e9f
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-24 17:50:21 -0700

    first version

---
+++ thirtynine.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   int n,i,*ptr,sum=0;
+   printf("Enter number of elements: ");
+   scanf("%d",&n);
+   ptr=(int*)calloc(n,sizeof(int));
+   if(ptr==NULL)
+   {
+      printf("Error! memory not allocated.");
+      exit(0);
+   }
+   printf("Enter elements of array: ");
+   for(i=0;i<n;++i)
+   {
+      scanf("%d",ptr+i);
+      sum+=*(ptr+i);
+   }
+   printf("Sum=%d\n",sum);
+   free(ptr);
+   return 0;
+}
+
+//This program is an example of calloc (contiguous memory allocation)
