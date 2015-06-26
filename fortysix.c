0ca326ee5a925d36cd0f562703c709d1c51a586c
SamhitaVasu <samhita.vasu@gmail.com>
2015-06-25 19:43:57 -0700

    First Version

---
+++ fortysix.c
@@ -0,0 +1,13 @@
+// First Program copy
+// =============
+#include <stdio.h>
+
+int main() 
+{
+   printf("Hello World!\n");
+// Print "Hello World";
+   return 0;
+// Return nothing
+}
+
+//Made a makefile for this program
